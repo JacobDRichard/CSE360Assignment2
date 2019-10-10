02758cee01b2936699a5487e207693b8431ef0ee
Jacob Richard <JacobDanielRichard@yahoo.com>
2019-10-09 21:48:20 -0700

    Initial commit

---
+++ CSE 360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360assign2;
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine() {
+		total = 0;
+	}
+	
+	/**
+	 * Returns the current total of the AddingMachine
+	 *
+	 * @return      The total that the AddingMachine holds
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Adds 'value' to the total
+	 *
+	 * @param	value	Integer to be added to the total
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Subtracts 'value' from the total
+	 *
+	 * @param  value	Integer to be subtracted from the total
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/**
+	 * Returns the current list of operations
+	 *
+	 * @return      The current list of operations the AddingMachine holds
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Clears the current list of operations and values that the AddingMachine holds
+	 *
+	 */
+	public void clear() {
+		
+	}
+}
